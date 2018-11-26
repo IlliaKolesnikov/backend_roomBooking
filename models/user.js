2e1d8faac4319e59357930529c1411837d177568
Illia Kolesnikov <kolesnikov00000@gmail.com>
2018-11-26 17:07:27 +0200

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Schema = mongoose.Schema
+let bcrypt_cost = 12
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  passwordHash: String,
+  isActivated: Boolean,
+  activationKey: String,
+})
+
+userSchema.statics.hashPassword = (passwordRaw, cb) => {
+  if (process.env.NODE_ENV === 'test') {
+    bcrypt_cost = 1
+  }
+  bcrypt.hash(passwordRaw, bcrypt_cost, cb)
+}
+
+userSchema.statics.comparePasswordAndHash = (password, passwordHash, cb) => {
+  bcrypt.compare(password, passwordHash, cb)
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
---
+++ server.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors')
+const logger = require("morgan");
+const sign = require("./routes/sign.js")
+
+
+const API_PORT = 8080;
+const app = express();
+
+// this is our MongoDB database
+const dbRoute = "mongodb://incode_server:incode1@ds157503.mlab.com:57503/incode_server";
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+
+
+app.use("/api", sign);
+
+
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
